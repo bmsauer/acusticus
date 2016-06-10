357e4dbd59ae36fe4bdf6340edc705ab6e727626
Brian Sauer <brianmsauer@gmail.com>
2016-06-10 17:03:40 -0400

    Got a new action bean loaded

---
+++ quickstart/tmp.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="stripes" uri="http://stripes.sourceforge.net/stripes.tld"%>
+ 
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+<head><title>Test</title></head>
+<body>
+<h1>Test</h1>
+</body>
+</html> 
---
+++ src/AcusticusAPIActionBean.java
@@ -0,0 +1,27 @@
+package net.sauertek.acusticus.web.api;
+ 
+import net.sourceforge.stripes.action.DefaultHandler;
+import net.sourceforge.stripes.action.Resolution;
+import net.sourceforge.stripes.action.ForwardResolution;
+import net.sourceforge.stripes.action.ActionBean;
+import net.sourceforge.stripes.action.ActionBeanContext;
+ 
+/**
+* Acustic.us API
+* @author Brian Sauer
+*/
+public class AcusticusAPIActionBean implements ActionBean {
+    private ActionBeanContext context;
+ 
+    public ActionBeanContext getContext() { return context; }
+    public void setContext(ActionBeanContext context) { this.context = context; }
+    /*
+    @DefaultHandler
+    public Resolution tmp() {
+        return new ForwardResolution("/quickstart/tmp.jsp");
+	}*/
+    @DefaultHandler
+    public String tmp(){
+	return "{'name':'hello'}";
+    }
+} 
