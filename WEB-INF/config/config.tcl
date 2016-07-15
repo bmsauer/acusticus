bcafce1cf9044c1472c21d6f729e8a48f7c7000a
Brian Sauer <brianmsauer@gmail.com>
2016-07-14 23:06:07 -0400

    added tcl script to generate ini file

---
+++ WEB-INF/config/config.tcl
@@ -0,0 +1,17 @@
+package require Tcl 8.5
+package require inifile 0.3
+
+#config.tcl is a script to generate .ini config files
+#based on environment.  It pulls variables from
+#system environment variables.
+
+set fileout [::ini::open config.ini w]
+
+foreach {name value} [array get ::env] {
+    if {[string match ACUSTICUS_* $name]} then {
+	::ini::set $fileout acusticus $name $value
+    } else {}
+}
+
+::ini::commit $fileout
+::ini::close $fileout
